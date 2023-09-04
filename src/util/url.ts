b9fd97eb6751d1ab00f1b20014e4af734a3bedf4
Jesse205 <2055675594@qq.com>
2023-09-05 04:46:27 +0800

    修复服务端路径问题

--- src/util/url.ts
+++ src/util/url.ts
@@ -1,3 +1,15 @@
-export function getCompletePath(url: string, base?: string) {
+/*export function getCompletePath(url: string, base?: string) {
   return new URL(url, base).href
+}*/
+
+/**
+ * 服务器的相对路径为绝对路径，因此不能使用new URL(url, base).href
+ * @param url
+ * @param base
+ * @returns
+ */
+export function getCompletePath(url: string, base: string) {
+  if (url.search('://') > -1)
+    return url
+  return `${base}/${url}`
 }
