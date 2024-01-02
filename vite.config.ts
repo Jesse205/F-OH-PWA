c6779da29536246936e3597dec1919dca35e3efc
Jesse205 <2055675594@qq.com>
2024-01-02 21:09:25 +0800

    refactor: 删除vuei18n别名

--- vite.config.ts
+++ vite.config.ts
@@ -47,7 +47,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
     },
     extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   },
