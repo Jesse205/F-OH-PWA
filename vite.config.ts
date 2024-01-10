9ab6712f4614960c2873f8dce508ef7e817cd772
Jesse205 <2055675594@qq.com>
2024-01-11 05:36:48 +0800

    chore: 微小的修改

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     vue({
       template: { transformAssetUrls },
     }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin
     vuetify({
       autoImport: true,
     }),
