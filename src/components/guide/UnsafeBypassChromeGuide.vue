f2386daba6d8e2a19cf61e75e4550997e132da0b
Jesse205 <2055675594@qq.com>
2024-08-11 19:09:40 +0800

    feat: 图片·添加比例

--- src/components/guide/UnsafeBypassChromeGuide.vue
+++ src/components/guide/UnsafeBypassChromeGuide.vue
@@ -30,6 +30,7 @@ defineExpose({
         ref="exampleImgElement"
         class="bypass-example__image border rounded-md"
         src="@/assets/images/guide/unsafe-bypass/chrome-flags-insecure-origins.svg"
+        :aspect-ratio="1.277258566978193"
       />
       <span class="bypass-example__content">{{ currentApiUrl }}</span>
     </div>
