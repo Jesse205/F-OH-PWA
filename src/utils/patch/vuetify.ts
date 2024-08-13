d5a1d3229f2d51747ac84bb128e8704c17c2ce19
Jesse205 <2055675594@qq.com>
2024-08-14 00:18:38 +0800

    pref: 优化fixVRippleOnMobile，尝试解决qq无法修复的问题

--- src/utils/patch/vuetify.ts
+++ src/utils/patch/vuetify.ts
@@ -1,16 +1,15 @@
-import { isChrome } from '@/utils/browser'
 
+/**
+ * 在触屏设备上，当用户点击应用了波纹的元素时，波纹不能正确地触发，而是直接显示动画后的状态。通过刻意延迟 `setTimeout` 的 `timeout` 为 0 的函数（插入完成状态的元素），等待dom更新时
+ */
 function fixVRippleOnMobile() {
-  if (!isChrome) {
-    return
-  }
   const originSetTimeout = setTimeout
   window.setTimeout = function (handler: TimerHandler, timeout?: number): number {
-    if (timeout === 0) {
+    if (timeout === 0 && typeof handler === 'function') {
       return originSetTimeout(() => {
         requestAnimationFrame(() => {
           requestAnimationFrame(() => {
-            originSetTimeout(handler, 0)
+            handler()
           })
         })
       }, 0)
