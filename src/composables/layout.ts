c9187e397b269cfbbff1b5e98004b5e645961441
Jesse205 <2055675594@qq.com>
2024-08-28 02:11:00 +0800

    pref: useLandscapeLayout删除无用判断

--- src/composables/layout.ts
+++ src/composables/layout.ts
@@ -5,7 +5,7 @@ export function useLandscapeLayout() {
   const { width, height } = useWindowSize()
   const isLandscapeNeeded = computed(() => {
     const ratio = width.value / height.value
-    return (ratio < 3 / 4 || ratio > 4 / 3) && width.value >= height.value
+    return ratio > 4 / 3
   })
   return { isLandscapeNeeded }
 }
